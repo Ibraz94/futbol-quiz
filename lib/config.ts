a2b25eda74fae4e01b4d6884828b7ebd3af5ef66
Izaan Sohail <izaansohail1999@gmail.com>
2025-08-13 20:00:12 +0500

    Resolved backend issue

--- lib/config.ts
+++ lib/config.ts
@@ -1,2 +1,2 @@
-// export const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || "https://api.futbolquiz.staging.pegasync.com";
-export const API_BASE_URL = "http://localhost:4010";
+export const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || "https://api.futbolquiz.staging.pegasync.com";
+// export const API_BASE_URL = "http://localhost:4010";
